74bcf549bc1164ccff3207ff43b0684481cca07c
Shaswat-glitch <132404838+Shaswat-glitch@users.noreply.github.com>
2023-06-10 22:38:55 +0530

    Add files via upload

---
+++ conditionalAndLoops.js
@@ -0,0 +1,223 @@
+/**
+ * Returns the 'Fizz','Buzz' or an original number using the following rules:
+ * 1) return original number
+ * 2) but if number multiples of three return 'Fizz'
+ * 3) for the multiples of five return 'Buzz'
+ * 4) for numbers which are multiples of both three and five return 'FizzBuzz'
+ *
+ * @param {number} num
+ * @return {any}
+ *
+ * @example
+ *   2 =>  2
+ *   3 => 'Fizz'
+ *   5 => 'Buzz'
+ *   4 => 4
+ *  15 => 'FizzBuzz'
+ *  20 => 'Buzz'
+ *  21 => 'Fizz'
+ *
+ */
+function getFizzBuzz(num) {
+	if(num%3==0 && (num%5)!=0){
+		return 'Fizz';
+	}
+	else if(num%5==0 && (num%3)!=0){
+		return 'Buzz';
+	}
+	else if(num%3==0 && num%5==0){
+		return 'FizzBuzz';
+	}
+	else{
+		return num;
+	}
+}
+
+/**
+ * Returns the factorial of the specified integer n.
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   1  => 1
+ *   5  => 120
+ *   10 => 3628800
+ */
+function getFactorial(n) {
+	var fac=1;
+	for(let i=1;i<=n;i++){
+		fac*=i;
+	}
+	return fac;
+}
+
+/**
+ * Returns the sum of integer numbers between n1 and n2 (inclusive).
+ *
+ * @param {number} n1
+ * @param {number} n2
+ * @return {number}
+ *
+ * @example:
+ *   1,2   =>  3  ( = 1+2 )
+ *   5,10  =>  45 ( = 5+6+7+8+9+10 )
+ *   -1,1  =>  0  ( = -1 + 0 + 1 )
+ */
+function getSumBetweenNumbers(n1, n2) {
+	var sum=0;
+	for(let i=n1;i<=n2;i++){
+       sum+=i;
+	}
+	return sum;
+}
+
+/**
+ * Returns true, if a triangle can be built with the specified sides a,b,c and false
+ * in any other ways.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {bool}
+ *
+ * @example:
+ *   1,2,3    =>  false
+ *   3,4,5    =>  true
+ *   10,1,1   =>  false
+ *   10,10,10 =>  true
+ */
+function isTriangle(a, b, c) {
+	var x=Math.max(a,b,c);
+	var y= a+b+c -x;
+	if(y>x) return true;
+	else return false;
+}
+
+/**
+ * Reverse the specified string (put all chars in reverse order)
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
+ * 'abracadabra' => 'arbadacarba'
+ * 'rotator' => 'rotator'
+ * 'noon' => 'noon'
+ */
+function reverseString(str) {
+	const revArray = [];
+	const length = str.length - 1;
+	  
+	for(let i = length; i >= 0; i--) {
+		revArray.push(str[i]);
+	}
+	return revArray.join('');
+}
+
+
+/**
+ * Returns true if the specified string has the balanced brackets and false otherwise.
+ * Balanced means that is, whether it consists entirely of pairs of opening/closing brackets
+ * (in that order), none of which mis-nest.
+ * Brackets include [],(),{},<>
+ *
+ * @param {string} str
+ * @return {boolean}
+ *
+ * @example:
+ *   '' => true
+ *   '[]'  => true
+ *   '{}'  => true
+ *   '()   => true
+ *   '[[]' => false
+ *   ']['  => false
+ *   '[[][][[]]]' => true
+ *   '[[][]][' => false
+ *   '{)' = false
+ *   '{[(<{[]}>)]}' = true
+ */
+function isBracketsBalanced(str) {
+	throw new Error("Not implemented");
+}
+
+/**
+ * Returns the human readable string of time period specified by the start and end time.
+ * The result string should be constrcuted using the folliwing rules:
+ *
+ * ---------------------------------------------------------------------
+ *   Difference                 |  Result
+ * ---------------------------------------------------------------------
+ *    0 to 45 seconds           |  a few seconds ago
+ *   45 to 90 seconds           |  a minute ago
+ *   90 seconds to 45 minutes   |  2 minutes ago ... 45 minutes ago
+ *   45 to 90 minutes           |  an hour ago
+ *  90 minutes to 22 hours      |  2 hours ago ... 22 hours ago
+ *  22 to 36 hours              |  a day ago
+ *  36 hours to 25 days         |  2 days ago ... 25 days ago
+ *  25 to 45 days               |  a month ago
+ *  45 to 345 days              |  2 months ago ... 11 months ago
+ *  345 to 545 days (1.5 years) |  a year ago
+ *  546 days+                   |  2 years ago ... 20 years ago
+ * ---------------------------------------------------------------------
+ *
+ * @param {Date} startDate
+ * @param {Date} endDate
+ * @return {string}
+ *
+ * @example
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:00.200')  => 'a few seconds ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-01 01:00:05.000')  => '5 minutes ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2000-01-02 03:00:05.000')  => 'a day ago'
+ *   Date('2000-01-01 01:00:00.100'), Date('2015-01-02 03:00:05.000')  => '15 years ago'
+ *
+ */
+function timespanToHumanString(startDate, endDate) {
+	throw new Error("Not implemented");
+}
+
+/**
+ * Returns the string with n-ary (binary, ternary, etc, where n<=10) representation of
+ * specified number.
+ * See more about
+ * https://en.wikipedia.org/wiki/Binary_number
+ * https://en.wikipedia.org/wiki/Ternary_numeral_system
+ * https://en.wikipedia.org/wiki/Radix
+ *
+ * @param {number} num
+ * @param {number} n, radix of the result
+ * @return {string}
+ *
+ * @example:
+ *   1024, 2  => '10000000000'
+ *   6561, 3  => '100000000'
+ *    365, 2  => '101101101'
+ *    365, 3  => '111112'
+ *    365, 4  => '11231'
+ *    365, 10 => '365'
+ */
+function toNaryString(num, n) {
+	var p =1;
+	var y=0;
+	var x;
+	while(num>0){
+		x=num%n;
+		y = y+p*x;
+		num = Math.floor(num/n);
+		p=p*10;
+		x=0;
+	}
+	return y.toString();
+}
+
+module.exports = {
+	getFizzBuzz,
+	getFactorial,
+	getSumBetweenNumbers,
+	isTriangle,
+	reverseString,
+	isBracketsBalanced,
+	timespanToHumanString,
+	toNaryString,
+};
---
+++ numbers.js
@@ -0,0 +1,116 @@
+/**
+ * Returns an area of a rectangle given by width and heigth.
+ *
+ * @param {numder} width
+ * @param {number} height
+ * @return {number}
+ *
+ * @example:
+ *   5, 10 => 50
+ *   5, 5  => 25
+ */
+function getRectangleArea(width, height) {
+	return width*height;
+}
+
+/**
+ * Returns an average of two given numbers.
+ *
+ * @param {numder} value1
+ * @param {number} value2
+ * @return {number}
+ *
+ * @example:
+ *   5, 5  => 5
+ *  10, 0  => 5
+ *  -3, 3  => 0
+ */
+function getAverage(value1, value2) {
+	return (value1/2+value2/2);
+}
+
+/**
+ * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ *
+ * @example:
+ *   5*x - 10 = 0    => 2
+ *   x + 8 = 0       => -8
+ *   5*x = 0         => 0
+ */
+function getLinearEquationRoot(a, b) {
+	if(b==0){
+		return 0;
+		}
+		else if(a!=0){
+		return (-1*(b/a));
+		}
+}
+
+/**
+ * Returns a last digit of a integer number.
+ *
+ * @param {number} value
+ * @return {number}
+ *
+ * @example:
+ *   100     => 0
+ *    37     => 7
+ *     5     => 5
+ *     0     => 0
+ */
+function getLastDigit(value) {
+	return(Math.abs(value%10));
+}
+
+/**
+ * Returns a number by given string representation.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example:
+ *    '100'     => 100
+ *     '37'     => 37
+ * '-525.5'     => -525.5
+ */
+function parseNumberFromString(value) {
+	return parseFloat(value);
+}
+
+/**
+ * Returns true is the number is prime; otherwise false.
+ * See: https://en.wikipedia.org/wiki/Primality_test
+ *
+ * @param {number} n
+ * @return {bool}
+ *
+ * @example:
+ *   4 => false
+ *   5 => true
+ *   6 => false
+ *   7 => true
+ *   11 => true
+ *   12 => false
+ *   16 => false
+ *   17 => true
+ */
+function isPrime(n) {
+	if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
+		var m = Math.sqrt(n); //returns the square root of the passed value
+		for (var i = 2; i <= m; i++)
+			if (n % i == 0) return false;
+		return true;
+}
+
+module.exports = {
+	getRectangleArea,
+	getAverage,
+	getLinearEquationRoot,
+	getLastDigit,
+	parseNumberFromString,
+	isPrime,
+};
